840895e58e7ef8e7090b028b6d20f2de96fefc10
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-02-09 11:39:39 +0000

    Adding yargs package

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 
 const request = require('request');
-
+const yargs = require('yargs');
+// google api key: 'AIzaSyAs2bVbCOCdLUN3defSZQ1wNsI4-XsR7js'
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=53%20burton%20road%20cottingham',
     json: true    
 }, function (error, response, body) {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log(body);
+    console.log(JSON.stringify(`Address: ${body.results[0].formatted_address}`, undefined, 2));
+    console.log(JSON.stringify(`Lattitude: ${body.results[0].geometry.location.lat}`, undefined, 2));
+    console.log(JSON.stringify(`longitude: ${body.results[0].geometry.location.lng}`, undefined, 2));
 });
-
-// key: 'AIzaSyAs2bVbCOCdLUN3defSZQ1wNsI4-XsR7js'
