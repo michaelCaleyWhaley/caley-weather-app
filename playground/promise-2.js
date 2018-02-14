ff222f7656fa9881d38c3f89f3df2573c0c01d39
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-02-14 16:56:37 +0000

    Testing promise with geolocation data

---
+++ playground/promise-2.js
@@ -0,0 +1,26 @@
+
+const request = require('request');
+
+var geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        address = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address);
+        request({
+            url: address,
+            json: true
+        }, (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                reject('Failed to gather address');
+            } else if(body.status === 'OVER_QUERY_LIMIT'){
+                reject('Over Google query limit');
+            } else {
+                resolve(body.results[0].formatted_address);
+            }
+        });
+    });
+};
+
+geocodeAddress('hu16').then((response) => {
+    console.log(response);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+});
--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,4 @@
+// Simple promise
 var somePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         // resolve('Hey. It worked!');
@@ -5,9 +6,29 @@ var somePromise = new Promise((resolve, reject) => {
     }, 2000);
 
 });
-
 somePromise.then((message) => {
     console.log(message);
 }).catch((message) => {
     console.log(message);
+});
+
+//advanced promise chaining
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 1500);
+    });
+};
+// to chain respose must be returned in .then
+asyncAdd(1, 2).then((response) => {
+    return asyncAdd(response, 3);
+}).then((response) => {
+    console.log(response);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
 });
